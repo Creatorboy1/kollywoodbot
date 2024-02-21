33a6df0d9eec4ba5940ae503d2fdbd5d08300b37
Creatorboy1 <74623750+Creatorboy1@users.noreply.github.com>
2024-02-21 20:06:30 +0000

    update 1

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
+import discord
+from discord.ext import commands
+import config
 
+
+bot = commands.Bot(command_prefix = '.', intents = discord.Intents.all())
+
+@bot.command()
+async def owner(ctx):
+  await ctx.send('My owner is <@724606248158232646> and this was done by github')
+
+@bot.command()
+async def shutdown(ctx):
+  await ctx.send('This bot is shutting down')
+  await bot.close()
+
+bot.run(config.token)
